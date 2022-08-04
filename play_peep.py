f9c5a918344164f62bdb1b3d5df71830825238dd
lunzard <60387314+lunzard@users.noreply.github.com>
2022-08-04 12:09:15 +0800

    create voice assistant peep music effect

---
+++ play_peep.py
@@ -0,0 +1,48 @@
+import glob
+import vlc
+import time
+
+# if the address is got directly from windows file explorer
+# format the backslash
+def format_playlist_address(address):
+    address.replace("\\" , "/")
+    address += "/*"
+    return address
+
+
+address = "./sound_effect/peep.mp3"
+inst = vlc.Instance()
+player = inst.media_list_player_new()
+playlist = [address]
+# for song in glob.glob(address):
+#     playlist.append(song)
+
+media_list = inst.media_list_new(playlist)
+# media_list = inst.media_list_new(["D:/NUS/project codes/Intel_Cup_voice_feedback/musiclist/Lyn - Last Surprise.mp3"])
+player.set_media_list(media_list)
+player.play()
+time.sleep(1)
+player.get_media_player().audio_set_volume(50)
+# time.sleep(1)
+
+# is_playing = True
+# while is_playing:
+#     volume = input("set Volume: ")
+#     # exit/stop
+#     if volume == '-1':
+#         is_playing = False
+#         print('exiting...')
+#         break
+#     # pause
+#     elif volume == '-2':
+#         player.set_pause(1)
+#     # resume/play
+#     elif volume == '-3':
+#         player.set_pause(0)
+#     # change volume
+#     else:
+#         print('set volume to ', volume)
+#         player.get_media_player().audio_set_volume(int(volume))
+
+print('finished play peep')
+    
